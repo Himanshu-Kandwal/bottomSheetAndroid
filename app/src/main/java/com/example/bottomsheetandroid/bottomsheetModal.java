7370584a44debd276e086fc46947fda5857d7a21
Himanshu-Kandwal <hkandwal50@gmail.com>
2020-12-23 03:59:31 +0530

    Added buttonclick to bottomsheetModal

--- app/src/main/java/com/example/bottomsheetandroid/bottomsheetModal.java
+++ app/src/main/java/com/example/bottomsheetandroid/bottomsheetModal.java
@@ -1,21 +1,58 @@
 package com.example.bottomsheetandroid;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import org.jetbrains.annotations.NotNull;
+
 public class bottomsheetModal extends BottomSheetDialogFragment {
-    final String TAG="BottomsheetModal";
+    final String TAG = "BottomsheetModal";
+
+    bottomSheetClickListner mbottomSheetClickListner; //instance for button callback
+
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.layout_bottomsheet_modal,container,false);
+        View bottomSheetView = inflater.inflate(R.layout.layout_bottomsheet_modal, container, false);
+
+//button1 click listner
+        bottomSheetView.findViewById(R.id.button1).setOnClickListener(v -> {
+            mbottomSheetClickListner.onBottomSheetButtonClick("Button1 Clicked");
+            dismiss();
+        });
+        //button1 click listner
+        bottomSheetView.findViewById(R.id.button2).setOnClickListener(v -> {
+            mbottomSheetClickListner.onBottomSheetButtonClick("Button2 Clicked");
+            dismiss();
+        });
+
+
+        return bottomSheetView;
+    }
+
+    interface bottomSheetClickListner {
+        void onBottomSheetButtonClick(String text);
+    }
+
+    @Override
+    public void onAttach(@NonNull @NotNull Context context) {
+        super.onAttach(context);
+        try {
+            mbottomSheetClickListner = (bottomSheetClickListner) context; //context is to reccieve method implementation of interface from activity
+        } catch (ClassCastException c) {
+            throw new ClassCastException("Must implement bottomSheetClickListner interface in host activity");
+        }
     }
 }
+
+
--- app/src/main/java/com/example/bottomsheetandroid/modalBottomSheet.java
+++ app/src/main/java/com/example/bottomsheetandroid/modalBottomSheet.java
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class modalBottomSheet extends AppCompatActivity {
+public class modalBottomSheet extends AppCompatActivity implements bottomsheetModal.bottomSheetClickListner {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modal_bottom_sheet);
+        textView = findViewById(R.id.textViewOutputModalBottomsheet);
 
-        Button button= findViewById(R.id.modalBottomSheetShow);
+        Button button = findViewById(R.id.modalBottomSheetShow);
         button.setOnClickListener(v -> {
-            bottomsheetModal bottomsheetModal= new bottomsheetModal();
-            bottomsheetModal.show(getSupportFragmentManager(),bottomsheetModal.TAG);
+            bottomsheetModal bottomsheetModal = new bottomsheetModal();
+            bottomsheetModal.show(getSupportFragmentManager(), bottomsheetModal.TAG);
         });
+    }
+
+    @Override
+    public void onBottomSheetButtonClick(String text) {
+        textView.setText(text);
     }
 }
